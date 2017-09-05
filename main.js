d2f707567837522579c370aba36d7e39969edf81
gdegas001 <gdegas001@gmail.com>
2017-09-05 16:11:58 -0700

    Finished code war

--- main.js
+++ main.js
@@ -1,39 +1,10 @@
-function printErrors(s) {
-  let numerator = 0
-  const length = s.length
-  const stringArray = s.split('')
-  for (let i = 0; i < stringArray.length; i++) {
-    if (stringArray[i].includes('n')) {
-      numerator++
-    } else if (stringArray[i].includes('n')) {
-      numerator++
-    } else if (stringArray[i].includes('o')) {
-      numerator++
-    } else if (stringArray[i].includes('p')) {
-      numerator++
-    } else if (stringArray[i].includes('q')) {
-      numerator++
-    } else if (stringArray[i].includes('r')) {
-      numerator++
-    } else if (stringArray[i].includes('s')) {
-      numerator++
-    } else if (stringArray[i].includes('t')) {
-      numerator++
-    } else if (stringArray[i].includes('u')) {
-      numerator++
-    } else if (stringArray[i].includes('v')) {
-      numerator++
-    } else if (stringArray[i].includes('w')) {
-      numerator++
-    } else if (stringArray[i].includes('x')) {
-      numerator++
-    } else if (stringArray[i].includes('y')) {
-      numerator++
-    } else if (stringArray[i].includes('z')) {
-      numerator++
-    } 
-  }
-  return numerator.toString() + '/' + length.toString()
+
+let friendsArray = ['boob', 'mange', 'lets', 'googgber']
+
+function friend(friends) {
+  return friends.filter(function(friend) {
+    return friend.length === 4
+  })
 }
 
-console.log(printErrors('abc'))
+console.log(friend(friendsArray))
